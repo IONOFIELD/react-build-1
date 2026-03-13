6231640a5429601547a5e18ebeceec87a9d1693d
Jayson <jaysonfield.jl@gmail.com>
2026-03-13 00:39:57 -0700

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ReactEEGApp from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ReactEEGApp />
-  </React.StrictMode>,
-)
